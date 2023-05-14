1cefb829af7a1379433b0bb27ff4db74f017bac9
Juan Pablo Ramos Torres <61593415+Juan-dev123@users.noreply.github.com>
2023-05-14 02:02:52 -0500

    test for get an animal by name (#14)

--- src/test/java/com/shelter/animalback/contract/api/animal/AnimalTest.java
+++ src/test/java/com/shelter/animalback/contract/api/animal/AnimalTest.java
@@ -76,13 +76,18 @@ public class AnimalTest {
         Mockito.when(animalService.save(any(Animal.class))).thenReturn(animal);
     }
 
-    @State("there are one animal")
+    @State("there are one animal to delete")
     public void deleteAnimals(){
-        AnimalDao animal = new AnimalDao();
+        Mockito.doNothing().when(animalService).delete(any(String.class));
+    }
+
+    @State("there one animal to return")
+    public void getAnimalByName(){
+        Animal animal = new Animal();
         animal.setName("Loli");
         animal.setBreed("Birmano");
         animal.setGender("Male");
         animal.setVaccinated(true);
-        Mockito.doNothing().when(animalService).delete(any(String.class));
+        Mockito.when(animalService.get(any(String.class))).thenReturn(animal);
     }
 }
